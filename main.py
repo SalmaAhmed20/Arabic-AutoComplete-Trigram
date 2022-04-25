7defbb3e59ed6a095ca8497028b1861fed3622d1
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2022-04-25 05:01:06 +0200

    read all files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import codecs
+import os
+
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -7,14 +8,22 @@ from nltk.corpus import stopwords
 
 if __name__ == '__main__':
     # Your directory to the folder with scraped websites
-    scraped_dir = 'Khaleej-2004/Economy/arc_Articlesww0c5e.html'
-    page=codecs.open(scraped_dir,encoding='utf-8')
-    for line in page:
-        print(line.strip())
-        sw = stopwords.words('arabic')
-        tokens = nltk.word_tokenize(line)
-        stopped_tokens = [i for i in tokens if not i in sw]
-        for item in stopped_tokens:
-            print(item)
+    scraped_dir = 'Khaleej-2004/Economy'
+    dataset=""
+    x=0
+    for filename in os.listdir(scraped_dir):
+        f = os.path.join(scraped_dir, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            page = open(f, encoding='utf-8')
+            dataset=dataset+page.read()
+    sw = stopwords.words('arabic')
+    tokens = nltk.word_tokenize(dataset)
+    stopped_tokens = [i for i in tokens if not i in sw]
+    print(stopped_tokens)
+    x=x+len(stopped_tokens)
 
+    print(x)
+        # for item in stopped_tokens:
+        #     print(item)
 
